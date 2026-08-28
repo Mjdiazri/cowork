e06633e01dc4867d18633bf1a5d7cc1232b0262b
Mjdiazri <citruss2012@hotmail.es>
2026-08-28 10:30:17 -0500

    Se crea service con metodos CRUD y enlace a repository

---
+++ src/main/java/com/generation/cowork/service/EspacioService.java
@@ -0,0 +1,49 @@
+package com.generation.cowork.service;
+
+import com.generation.cowork.model.Espacio;
+import com.generation.cowork.repository.EspacioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EspacioService {
+
+    //Variables
+    private EspacioRepository espacioRepository;
+
+    //Constructor
+    public EspacioService(EspacioRepository repository) {
+        this.espacioRepository = repository;
+    }
+
+    //Metodos Crud
+
+        //Create
+    public Espacio guardarNuevoEspacio(Espacio espacio){
+        return espacioRepository.guardar(espacio);
+    }
+
+        //Read all
+    public List<Espacio> ListarEspacios(){
+        return espacioRepository.encontrarTodo();
+    }
+
+        //Read one
+    public Optional<Espacio> ListarID(Long id){
+        return espacioRepository.encontrarId(id);
+    }
+
+        //Update
+    public Optional<Espacio> actualizar(Long id, Espacio datosEspacio){
+        return espacioRepository.actualizar(id,datosEspacio);
+    }
+
+        //Delete
+    public Boolean eliminarId(Long id){
+        return espacioRepository.eliminar(id);
+    }
+
+}
+
