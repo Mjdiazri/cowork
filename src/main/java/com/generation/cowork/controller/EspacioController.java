e9203dff25f1ebc97b7423d0b95742b93cadfb74
Mjdiazri <citruss2012@hotmail.es>
2026-08-28 13:29:35 -0500

    Se termina clase controller espacio con metodos crud

--- src/main/java/com/generation/cowork/controller/EspacioController.java
+++ src/main/java/com/generation/cowork/controller/EspacioController.java
@@ -2,10 +2,10 @@ package com.generation.cowork.controller;
 
 import com.generation.cowork.model.Espacio;
 import com.generation.cowork.service.EspacioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +30,36 @@ public class EspacioController {
     }
 
         //Get ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Espacio> listarID(@PathVariable Long id){
+        return espacioService.ListarID(id).map(espacio -> ResponseEntity.ok(espacio))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+        //Crear
+    @PostMapping
+    public ResponseEntity<Espacio> crearEspacio(@RequestBody Espacio espacio){
+        Espacio nuevoEspacio = espacioService.guardarNuevoEspacio(espacio);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoEspacio);
+    }
+
+        //Actualizar
+    @PutMapping("/{id}")
+    public ResponseEntity<Espacio> actualizarEspacio(@PathVariable Long id,@RequestBody Espacio espacio){
+    return espacioService.actualizar(id, espacio).map(espacio1 -> ResponseEntity.ok(espacio1))
+            .orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+        //Eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarEspacio(@PathVariable Long id){
+        if(espacioService.eliminarId(id)){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
 
 
 
